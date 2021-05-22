aaad5bbf3a49dba4bb2cddfce3acda312ec56dc2
gpbaculio <gpbaculio@gmail.com>
2021-05-22 17:22:52 +0800

    add boilerplate + Transform Animations

---
+++ src/TransformAnimations.tsx
@@ -0,0 +1,13 @@
+import "./TransformAnimations.css";
+
+import cloud from "./mario-examples/img/cloud.png";
+
+const TransformAnimations = () => {
+  return (
+    <div className='wrapper'>
+      <img src={cloud} />
+    </div>
+  );
+};
+
+export default TransformAnimations;
